080c30aef1e061416878b55769a199d570ebde70
Thomas Guillerme <guillert@tcd.ie>
2018-03-01 17:25:57 +1000

    zzz

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,4 @@
-# ## Package version checking
-# .onLoad <- function(libname = find.package("dispRity"), pkgname = "dispRity") {
-
-#     ## Check connection
-#     connection <- RCurl::url.exists("https://raw.githubusercontent.com/TGuillerme/dispRity/release/DESCRIPTION")
-
-#     ## Check version
-#     if(connection) {
-#         last_release <- as.numeric_version(strsplit(strsplit(RCurl::getURL("https://raw.githubusercontent.com/TGuillerme/dispRity/release/DESCRIPTION", followlocation = TRUE), split = "\nVersion: ")[[1]][2], split = "\nDate:")[[1]][1])
-#         current_version <- utils::packageVersion("dispRity")
-#         if(current_version < last_release) {
-#             packageStartupMessage(paste("A newer released version of dispRity (", last_release, ") is available on GitHub.\nTo get the most up to date version, you can use:\n    devtools::install_github(\"TGuillerme/dispRity\", branch = \"release\")", sep = ""))
-#         }
-#     }
-# }
+## Package version checking
+.onLoad <- function(libname = find.package("dispRity"), pkgname = "dispRity") {
+    packageStartupMessage(paste0("For the latest news, vignettes and releases,\ncheck on https://github.com/TGuillerme/dispRity"))
+}
