30e02f870336de4801f9b3af52813b3c3d5ac405
TGuillerme <guillert@tcd.ie>
2018-10-16 15:35:15 +1000

    factorising data function

---
+++ R/lda.test_fun.R
@@ -0,0 +1,93 @@
+## Factorising subsets from dispRity objects
+factorise.subsets <- function(data) {
+
+    ## Function for splitting the vector by categories
+    split.category <- function(category, split_names) {
+        output <- lapply(split_names, function(x, category) ifelse(x[[1]] == category, x[[2]], NA), category)
+        return(as.vector(na.omit(unlist(output))))
+    }
+
+    ## Function for translating one category (unique)
+    translate.category.all <- function(category, level, n_elements, subsets_list) {
+        ## Empty vector out
+        vector_out <- rep(NA, n_elements)
+        
+        ## Get the sublist of interest
+        if(!is.null(category)) {
+            sub_list <- unlist(subsets_list[grep(paste0(category, "."), names(subsets_list))], recursive = FALSE)
+        } else {
+            sub_list <- unlist(subsets_list, recursive = FALSE)
+        }
+
+        ## Loop through the replacement elements
+        for(one_level in 1:length(sub_list)) {
+            vector_out[sub_list[[one_level]]] <- level[one_level]
+        }
+
+        return(vector_out)
+    }
+
+    ## Function for translating one category (not unique)
+    translate.category.binary <- function(category, n_elements, subsets_list) {
+        ## Empty vector out
+        vector_out <- rep(NA, n_elements)
+
+        ## Level name
+        level <- names(subsets_list)[category]
+
+        ## Translate
+        vector_out[subsets_list[[category]]$elements] <- 1
+        vector_out[-subsets_list[[category]]$elements] <- 0
+        return(vector_out)
+    }
+
+    ## Get the number of elements to subset
+    n_elements <- dim(data$matrix)[1]
+
+    ## Get the subsets_list
+    subsets_list <- lapply(data$subsets, lapply, as.vector)
+
+    ## Get the number of actual elements
+    element_names <- names(subsets_list)
+
+    ## Check whether it is a dot named list
+    is_dot_named_list <- ifelse(length(grep("\\.", element_names)) == length(element_names), TRUE, FALSE)
+
+    ## Check if the number of categories is unique
+    length_unique <- length(unlist(subsets_list))
+    are_unique <- ifelse(length_unique > n_elements, FALSE, TRUE)
+
+    ## If not unique, check whether it is because of multiple categories
+    if(!are_unique && is_dot_named_list) {
+        are_unique <- ifelse(length_unique/n_elements == as.integer(length_unique/n_elements), TRUE, FALSE)
+    }
+
+    ## Each category as the right number of elements
+    if(are_unique) {
+        ## Named categories
+        if(is_dot_named_list) {
+            ## Split the names
+            split_names <- strsplit(element_names, split = "\\.")
+
+            ## Get the number of categories
+            categories <- unique(unlist(lapply(split_names, function(x) return(x[[1]]))))
+
+            ## Splitting by categories
+            levels <- lapply(as.list(categories), split.category, split_names)
+
+            ## Translate all categories
+            subsets_out <- mapply(translate.category.all, as.list(categories), levels, MoreArgs = list(n_elements, subsets_list), SIMPLIFY = FALSE)
+            names(subsets_out) <- categories
+
+            return(subsets_out)
+        } else {
+            ## Translate a unique non-named list
+            return(list(translate.category.all(category = NULL, level = element_names, n_elements, subsets_list)))
+        }
+    } else {
+        ## Create binary categories
+        output <- lapply(as.list(1:length(subsets_list)), translate.category.binary, n_elements, subsets_list)
+        names(output) <- names(subsets_list)
+        return(output)
+    }
+}
--- tests/testthat/test-lda.test.R
+++ tests/testthat/test-lda.test.R
@@ -1,5 +1,46 @@
 context("lda.test")
 
+## Factorise subsets
+test_that("factorise.subsets works", {
+
+    ## Simple factorisation return
+    ordinated_matrix <- matrix(data = rnorm(90), nrow = 10)
+    expect_warning(data <- custom.subsets(ordinated_matrix, list(c(1:4), c(5:10))))
+    test <- factorise.subsets(data)
+    expect_is(test, "list")
+    expect_equal(unlist(lapply(test, length)), 10)
+    expect_equal(unname(unlist(lapply(test, unique))), c("1", "2"))
+
+    ## Double elements factorisation return
+    require(geomorph)
+    data(plethodon)
+    procrustes <- geomorph::gpagen(plethodon$land)
+    geomorph_df <- geomorph::geomorph.data.frame(procrustes, species = as.factor(lda_test_data$species), morpho = as.factor(lda_test_data$morpho))
+    data <- geomorph.ordination(geomorph_df, ordinate = FALSE)
+
+    test <- factorise.subsets(data)
+    expect_is(test, "list")
+    expect_equal(names(test), c("species", "morpho"))
+    expect_equal(unlist(lapply(test, length)), c("species" = 40, "morpho" = 40))
+    expect_equal(unname(unlist(lapply(test, unique))), c("Jord", "Teyah", "group1", "group2", "group3"))
+
+
+    ## Complex factorisation (with NAs)
+    ordinated_matrix <- matrix(data = rnorm(90), nrow = 10,
+         dimnames = list(letters[1:10]))
+    data <- custom.subsets(ordinated_matrix,
+         list("A" = c("a", "b", "c", "d"), "B" = c("e", "f", "g", "h", "i", "j"),
+              "C" = c("a", "c", "d", "f", "h")))
+
+    test <- factorise.subsets(data)
+    expect_is(test, "list")
+    expect_equal(names(test), c("A", "B", "C"))
+    expect_equal(unlist(lapply(test, length)), c("A" = 10, "B" = 10, "C" = 10))
+    expect_equal(unname(unlist(lapply(test, unique))), c(1, 0, 0, 1, 1, 0))
+})
+
+
+
 ## Test
 test_that("lda.test works", {
 
